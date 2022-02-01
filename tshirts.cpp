ead3e6fd38be4b05904ff6c3531601ae31f6e3ff
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-02-01 19:47:30 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -5,7 +5,7 @@ char size(int cms) {
     char sizeName = '\0';
     if(cms <= 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms > 38 && cms <= 42) {
         sizeName = 'M';
     } else if(cms >= 42) {
         sizeName = 'L';
@@ -17,8 +17,8 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(38) == 'S');//assuming 38 as small
-    assert(size(42) == 'L');//assuming 42 as Large
+    assert(size(38) == 'S');
+    assert(size(42) == 'M');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
